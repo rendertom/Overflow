c7ba53d19fc324bb9d2d49ef5de93742b6def54e
Tomas Sinkunas <Tomas@rendertom.com>
2019-11-08 10:20:08 +0200

    Adds missing method

--- src/lib/ArrayEx.js
+++ src/lib/ArrayEx.js
@@ -9,6 +9,12 @@ var ArrayEx = (function() {
 		return array;
 	};
 
+	module.forEach = function(array, callback) {
+		for (var i = 0, il = array.length; i < il; i++) {
+			callback(array[i], i, array);
+		}
+	};
+
 	module.isArray = function(array) {
 		return Object.prototype.toString.call(array) === '[object Array]';
 	};
